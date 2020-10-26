e5e2851075fc6e307dedf8a05deece31e502b3ca
Marko Mikulicic <mkmik@vmware.com>
2020-10-26 18:18:55 +0100

    Change array slicing specification
    Features of the specification approach:
    1. defines array indexing in terms of the slicing operation,
       thus avoiding duplication
    2. albeit being verbose, the distinct forward and backward
       iteration loops and explicit lower and upper bound variable
       names helps the reader understand the termination condition.
    3. clearly distinguishes the "slice parameters" (which can be
       negative), from the normalized bounds (lower, upper) to help
       readers avoid the confusion of iterating until normalized
       end = -1.
    
    Co-authored-by: Glyn Normington <normingtong@vmware.com>

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -7,6 +7,15 @@ readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$script_dir/.."
 
-xml2rfc ./draft-normington-jsonpath-latest.xml --text --html && aex draft-normington-jsonpath-latest.txt | bap -S path -q && cp draft-normington-jsonpath-latest.html docs/index.html
-
+xml2rfc ./draft-normington-jsonpath-latest.xml --text --html && \
+aex draft-normington-jsonpath-latest.txt \
+| grep -v "lower =" \
+| grep -v "upper =" \
+| grep -v "i =" \
+| grep -v "step =" \
+| grep -v "step &gt;=" \
+| grep -v "start =" \
+| grep -v "end =" \
+| bap -S path -q && cp draft-normington-jsonpath-latest.html docs/index.html
+# TODO fix aex to not extract ABNF-looking rules from pseudocode blocks
 )
