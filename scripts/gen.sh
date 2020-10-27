8e99b8962c12e1bca9579e7745648f355823b045
Marko Mikulicic <mkmik@vmware.com>
2020-10-27 18:20:52 +0100

    Use xpath to feed only abnf soucecode to aex and bap
    thus avoding other types of sourcecode (such as pseudocode) to be validated by the ABNF checker.
    
    aex is still needed because it reformats the content for bap somehow (didn't investigate).

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -8,14 +8,7 @@ readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$script_dir/.."
 
 xml2rfc ./draft-normington-jsonpath-latest.xml --text --html && \
-aex draft-normington-jsonpath-latest.txt \
-| grep -v "lower =" \
-| grep -v "upper =" \
-| grep -v "i =" \
-| grep -v "step =" \
-| grep -v "step &gt;=" \
-| grep -v "start =" \
-| grep -v "end =" \
+xmlstarlet sel -T -t -v '//sourcecode[@type="abnf"]' ./draft-normington-jsonpath-latest.xml 2>/dev/null \
+| aex \
 | bap -S path -q && cp draft-normington-jsonpath-latest.html docs/index.html
-# TODO fix aex to not extract ABNF-looking rules from pseudocode blocks
 )
