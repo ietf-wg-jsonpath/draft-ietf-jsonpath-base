025f6c9a655b255faeec6326acb9e3c28686c66d
Glyn Normington <normingtong@vmware.com>
2020-11-17 11:10:02 +0000

    Convert to markdown
    This will be easier to edit and review in PRs.
    
    Thanks to @cabo for translating the XML file to suitable markdown and
    for creating the kramdown-based tooling. Also he fixed the outdated
    BCP14 boilerplate.
    
    Fixes https://github.com/jsonpath-standard/internet-draft/issues/39

--- scripts/gen.sh
+++ scripts/gen.sh
@@ -7,8 +7,8 @@ readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$script_dir/.."
 
-xml2rfc ./draft-normington-jsonpath-latest.xml --text --html && \
-xmlstarlet sel -T -t -v '//sourcecode[@type="abnf"]' ./draft-normington-jsonpath-latest.xml 2>/dev/null \
+kdrfc -h3 draft-normington-jsonpath-latest.md && \
+xmlstarlet sel -T -t -v '//artwork[@type="abnf"]' ./draft-normington-jsonpath-latest.xml 2>/dev/null \
 | aex \
 | bap -S path -q && cp draft-normington-jsonpath-latest.html docs/index.html
 )
