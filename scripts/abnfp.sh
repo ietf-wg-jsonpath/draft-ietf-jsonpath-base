b7b7becf431173c6abfb33472e11a71a09d873f9
Carsten Bormann <cabo@tzi.org>
2023-01-31 14:21:13 +0100

    abnfp: keep unprocessed ABNF around (indentation)

--- scripts/abnfp.sh
+++ scripts/abnfp.sh
@@ -2,6 +2,7 @@ LANG=C
 export LANG
 
 xmlstarlet sel -T -t -v '//sourcecode[@type="abnf"]' ./draft-ietf-jsonpath-base.xml 2>/dev/null \
+| tee draft-ietf-jsonpath-base-orig.abnf \
 | aex \
 | tee draft-ietf-jsonpath-base.abnf \
 | bap -S path
